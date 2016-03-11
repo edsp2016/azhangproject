002de231ee76fdd47e3ef1fa13992215fb9f0b51
Yoav Bergner <ybergner@gmail.com>
2016-03-11 13:33:45 -0500

    sample composer table added

--- sample_parse.r
+++ sample_parse.r
@@ -15,7 +15,35 @@ xmlSApply(firstchild, xmlName) #name(s)
 xmlSApply(firstchild, xmlSize) #size
 
 rootnode[[1]][["worksInfo"]][[1]][["workTitle"]]
+rootnode[[1]][["worksInfo"]][[1]][["composerName"]]
 xmlToList(rootnode[[1]][["worksInfo"]][[1]][["workTitle"]])
+
+
+incrementComp <- function(composer_stats, c, season){
+  if (is.null(composer_stats[c, season])) {
+    composer_stats[c, season] <- 1
+  } else if (is.na(composer_stats[c,season])) {
+    composer_stats[c, season] <- 1
+  } else {
+    composer_stats[c, season] <- composer_stats[c, season] + 1
+  }
+  return(composer_stats)
+}
+
+  
+composerBySeason <- data.frame()
+for (seas in 1:30) {
+  firstlist <- xmlToList(rootnode[[seas]])
+  season <- firstlist$season
+  season <- paste("Season",season,sep=".")
+  works <- firstlist$worksInfo
+  for (i in 1:length(works)) {
+    if (!is.null(works[[i]]$composerName)) {
+      composerBySeason <- incrementComp(composerBySeason, works[[i]]$composerName,season)
+    }
+  }
+}
+
 
 # Parsing the whole thing first is WARNING: SLOW
 # xml_aslist <- xmlToList(xmlfile)

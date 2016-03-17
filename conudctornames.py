aaa0e11ba6d0de194edd11c25d5cd8abcdfd87b4
azhangpanda <aiyizhang@gmail.com>
2016-03-17 19:24:26 -0400

    update

--- conudctornames.py
+++ conudctornames.py
@@ -2,19 +2,13 @@ import urllib2
 import numpy
 from bs4 import BeautifulSoup
 
-#page = urllib2.urlopen("https://en.wikipedia.org/wiki/Category:Conductors_(music)_by_nationality").read()
 page = urllib2.urlopen("https://en.wikipedia.org/wiki/Category:Classical_composers_by_nationality").read()
 soup = BeautifulSoup(page,"html.parser")
 prettify=soup.prettify()
 nationality={}
 test=soup.find_all("a", class_= "CategoryTreeLabel CategoryTreeLabelNs14 CategoryTreeLabelCategory")
 test=test[1:-1]
-#nationality={}
-#prefix="https://en.wikipedia.org/"
-#for element in test:
-#    nationality[element.get_text()] = {}
-#    nationality[element.get_text()["link"]]=prefix+element["href"]
-  
+ 
 nationality=[]
 page=[]
 nationalitypage=[]
@@ -23,11 +17,12 @@ for element in test:
     nationality.append(element.get_text().split(" ")[0])
     page.append(prefix+element["href"]) 
 nationalitypage=numpy.column_stack((nationality, page))
-#page1=urllib2.urlopen("https://en.wikipedia.org/wiki/Category:Russian_conductors_(music)").read()
-#soup1 = BeautifulSoup(page1,"html.parser")
-#test1=soup1.find_all("li").find_all("a")
 
-#<li><a href="/wiki/Valery_Afanassiev" title="Valery Afanassiev">Valery Afanassiev</a></li>
-#<a class="CategoryTreeLabel CategoryTreeLabelNs14 CategoryTreeLabelCategory" href="/wiki/Category:Uruguayan_conductors_(music)">Uruguayan conductors (music)</a>
+test2=[]
+for i in range(0,len(nationalitypage)):
+    page1=urllib2.urlopen(nationalitypage[i][1])
+    soup1 = BeautifulSoup(page1,"html.parser")
+    test1=soup1.find_all("a")
+    for element in test1:
+        test2.append(element.get_text())
 
-print nationalitypage
